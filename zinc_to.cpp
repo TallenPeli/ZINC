455d9fb78485f48d51e9583f3f77e4a96c954ea4
Talleeenos69 <talleeenos69@gmail.com>
2023-10-11 15:08:12 -0400

    added examples

--- zinc_to.cpp
+++ zinc_to.cpp
@@ -13,6 +13,26 @@ std::string input(std::string prompt){std::string Input;std::cout << prompt;std:
 std::string getline(std::string prompt){std::string Input;std::cout << prompt;getline(std::cin, Input);return(Input);}
 
 
+std::string word = "abcdefghijklmnopqrstuvwxyz";
+
+char* word_list = new char[word.length()];
+
 int main(){
-    print("Hello, World!");
+    for(int  a = 0;  a < word.length();  a++){
+        word_list[a] = word[a];
+    }
+
+    for(int  i = 0;  i < word.size();  i++){
+        for(int  j = 0;  j < i;  j++){
+            print(word_list[j]);
+        }
+        print(" ");
+        for(int  k = 0;  k < abs(i-word.size());  k++){
+            print(word_list[k]);
+        }
+        newline();
+    }
+
+    // Don't forget to release the allocated memory when done
+    delete[] word_list;
 }
