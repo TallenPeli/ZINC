204a9c031d9c86d43d1a1430dabc7aabd7f9b541
Talleeenos69 <talleeenos69@gmail.com>
2023-10-18 08:49:41 -0400

    zinc_to

---
+++ zinc_to.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+//Standard Zinc functions from zincstd
+template <typename T>
+void print(const T& input){std::cout << input;}
+void print(std::string input){std::cout << input;}
+template <typename T>
+void println(const T& input = T()) { if constexpr (!std::is_void_v<T>) std::cout << input << std::endl; }
+void println(){std::cout << std::endl;}
+void newline(){std::cout << std::endl;}
+std::string input(std::string prompt){std::string Input;std::cout << prompt;std::cin >> Input;return Input;}
+std::string getline(std::string prompt){std::string Input;std::cout << prompt;getline(std::cin, Input);return(Input);}
+
+
+std::string word = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+<>?:{}|";
+
+std::string word_list[56];
+
+int main(){
+    for(int  a = 0;  a < word.length();  a++){
+        word_list[a] = word[a];
+    }
+
+    for(int  i = 0;  i < word.size();  i++){
+        for(int  j = 0;  j < i;  j++){
+            print(word_list[abs(i-word.length())]);
+        }
+	print(" ");
+        for(int  k = 0;  k < abs(i-word.size());  k++){
+            print(word_list[k]);
+        }
+	newline();
+    }
+}
